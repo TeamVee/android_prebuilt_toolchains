73a55e95f7bc63bd02d617d8daad2f4b206c749d
Caio Oliveira <caiooliveirafarias0@gmail.com>
2015-11-22 14:04:23 -0200

    Update APTESS and README
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- aptess.sh
+++ aptess.sh
@@ -5,12 +5,12 @@
 # Linaro GCC Toolchains Branch
 
 echo "1) GCC 4.8.4 (2014.11) toolchain from Linaro"
-echo "2) GCC 4.9.3 (2015.03) toolchain from Linaro"
+echo "2) GCC 4.9.4 (2015.06) toolchain from Linaro"
 unset errortoolchain
 read -p "Choice: " -n 1 -s toolchain
 case "$toolchain" in
 	1 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-linux-gnueabi-linaro_4.8.4-2014.11/bin/arm-linux-gnueabi-"; ToolchainCompile="Linaro GCC 4.8.4";;
-	2 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-cortex-linux-gnueabi-linaro_4.9.3-2015.03/bin/arm-cortex-linux-gnueabi-"; ToolchainCompile="Linaro GCC 4.9.3";;
+	2 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-cortex-linux-gnueabi-linaro_4.9.4-2015.06/bin/arm-cortex-linux-gnueabi-"; ToolchainCompile="Linaro GCC 4.9.4";;
 	* ) echo "$toolchain - This option is not valid"; sleep .5; errortoolchain="ON";;
 esac
 if ! [ "$errortoolchain" == "ON" ]; then
