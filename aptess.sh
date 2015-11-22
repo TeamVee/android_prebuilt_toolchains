45ecfa0420e60067fe63301f6d55b22d51950670
Caio Oliveira <caiooliveirafarias0@gmail.com>
2015-11-22 12:07:20 -0200

    Google GCC Toolchains Branch
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- aptess.sh
+++ aptess.sh
@@ -2,18 +2,15 @@
 
 # Android Prebuilt Toolchains External Script Set - APTESS
 # only for build ARM Linux Kernels
+# Google GCC Toolchains Branch
 
 echo "1) GCC 4.7 Toolchain from Google"
 echo "2) GCC 4.8 Toolchain from Google"
-echo "3) GCC 4.8.4 (2014.11) toolchain from Linaro"
-echo "4) GCC 4.9.3 (2015.03) toolchain from Linaro"
 unset errortoolchain
 read -p "Choice: " -n 1 -s toolchain
 case "$toolchain" in
 	1 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-eabi-4.7/bin/arm-eabi-"; ToolchainCompile="Google GCC 4.7";;
 	2 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-eabi-4.8/bin/arm-eabi-"; ToolchainCompile="Google GCC 4.8";;
-	3 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-linux-gnueabi-linaro_4.8.4-2014.11/bin/arm-linux-gnueabi-"; ToolchainCompile="Linaro GCC 4.8.4";;
-	4 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-cortex-linux-gnueabi-linaro_4.9.3-2015.03/bin/arm-cortex-linux-gnueabi-"; ToolchainCompile="Linaro GCC 4.9.3";;
 	* ) echo "$toolchain - This option is not valid"; sleep .5; errortoolchain="ON";;
 esac
 if ! [ "$errortoolchain" == "ON" ]; then
