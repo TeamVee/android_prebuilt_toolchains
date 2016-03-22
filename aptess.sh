5743106b17c82d86bebd5c140cbd4cf3d4ee930a
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-03-22 19:42:49 -0300

    APTESS: Prevent error in call of build.sh

--- aptess.sh
+++ aptess.sh
@@ -18,8 +18,8 @@ read -p "Choice: " -n 1 -s x
 _CROSS_COMPILE=$CROSS_COMPILE
 unset CROSS_COMPILE
 case "$x" in
--	1 ) export CROSS_COMPILE="$(pwd)/android_prebuilt_toolchains/arm-linux-gnueabi-linaro_4.8.4-2014.11/bin/arm-linux-gnueabi-"; ToolchainCompile="Linaro GCC 4.8.4";;
--	2 ) export CROSS_COMPILE="$(pwd)/android_prebuilt_toolchains/arm-cortex-linux-gnueabi-linaro_4.9.4-2015.06/bin/arm-cortex-linux-gnueabi-"; ToolchainCompile="Linaro GCC 4.9.4";;
+-	1 ) export CROSS_COMPILE="$(pwd)/../android_prebuilt_toolchains/arm-linux-gnueabi-linaro_4.8.4-2014.11/bin/arm-linux-gnueabi-"; ToolchainCompile="Linaro GCC 4.8.4";;
+-	2 ) export CROSS_COMPILE="$(pwd)/../android_prebuilt_toolchains/arm-cortex-linux-gnueabi-linaro_4.9.4-2015.06/bin/arm-cortex-linux-gnueabi-"; ToolchainCompile="Linaro GCC 4.9.4";;
 	* ) ;;
 esac
 if [ "$CROSS_COMPILE" == "" ]
