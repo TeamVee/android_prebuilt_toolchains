0772d4fe542e093f425cc49ce56531fe84d2abdb
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-03-22 10:42:03 -0300

    APTESS: First update after a long time
    Can be called for any dir now

--- aptess.sh
+++ aptess.sh
@@ -4,15 +4,28 @@
 # only for build ARM Linux Kernels
 # Google GCC Toolchains Branch
 
+clear
+echo
+echo "-APTESS - Toolchain Choice-"
+echo
 echo "1) GCC 4.7 Toolchain from Google"
 echo "2) GCC 4.8 Toolchain from Google"
+echo
+echo "*) Any other key to Exit"
+echo
 unset errortoolchain
-read -p "Choice: " -n 1 -s toolchain
-case "$toolchain" in
-	1 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-eabi-4.7/bin/arm-eabi-"; ToolchainCompile="Google GCC 4.7";;
-	2 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-eabi-4.8/bin/arm-eabi-"; ToolchainCompile="Google GCC 4.8";;
-	* ) echo "$toolchain - This option is not valid"; sleep .5; errortoolchain="ON";;
+read -p "Choice: " -n 1 -s x
+_CROSS_COMPILE=$CROSS_COMPILE
+unset CROSS_COMPILE
+case "$x" in
+	1 ) export CROSS_COMPILE="$(pwd)/android_prebuilt_toolchains/arm-eabi-4.7/bin/arm-eabi-"; ToolchainCompile="Google GCC 4.7";;
+	2 ) export CROSS_COMPILE="$(pwd)/android_prebuilt_toolchains/arm-eabi-4.8/bin/arm-eabi-"; ToolchainCompile="Google GCC 4.8";;
+	* ) ;;
 esac
-if ! [ "$errortoolchain" == "ON" ]; then
-	echo "$ToolchainCompile"
+if [ "$CROSS_COMPILE" == "" ]
+then
+	CROSS_COMPILE=$_CROSS_COMPILE
+	unset _CROSS_COMPILE
+else
+	echo $ToolchainCompile
 fi
