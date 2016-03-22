303ca67401bd12044c17f5ff13ad082d40c39732
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-03-22 10:45:33 -0300

    APTESS: First update after a long time
    Can be called for any dir now

--- aptess.sh
+++ aptess.sh
@@ -4,15 +4,28 @@
 # only for build ARM Linux Kernels
 # Linaro GCC Toolchains Branch
 
+clear
+echo
+echo "-APTESS - Toolchain Choice-"
+echo
 echo "1) GCC 4.8.4 (2014.11) toolchain from Linaro"
 echo "2) GCC 4.9.4 (2015.06) toolchain from Linaro"
+echo
+echo "*) Any other key to Exit"
+echo
 unset errortoolchain
-read -p "Choice: " -n 1 -s toolchain
-case "$toolchain" in
-	1 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-linux-gnueabi-linaro_4.8.4-2014.11/bin/arm-linux-gnueabi-"; ToolchainCompile="Linaro GCC 4.8.4";;
-	2 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-cortex-linux-gnueabi-linaro_4.9.4-2015.06/bin/arm-cortex-linux-gnueabi-"; ToolchainCompile="Linaro GCC 4.9.4";;
-	* ) echo "$toolchain - This option is not valid"; sleep .5; errortoolchain="ON";;
+read -p "Choice: " -n 1 -s x
+_CROSS_COMPILE=$CROSS_COMPILE
+unset CROSS_COMPILE
+case "$x" in
+-	1 ) export CROSS_COMPILE="$(pwd)/android_prebuilt_toolchains/arm-linux-gnueabi-linaro_4.8.4-2014.11/bin/arm-linux-gnueabi-"; ToolchainCompile="Linaro GCC 4.8.4";;
+-	2 ) export CROSS_COMPILE="$(pwd)/android_prebuilt_toolchains/arm-cortex-linux-gnueabi-linaro_4.9.4-2015.06/bin/arm-cortex-linux-gnueabi-"; ToolchainCompile="Linaro GCC 4.9.4";;
+	* ) ;;
 esac
-if ! [ "$errortoolchain" == "ON" ]; then
-	echo "$ToolchainCompile"
+if [ "$CROSS_COMPILE" == "" ]
+then
+	CROSS_COMPILE=$_CROSS_COMPILE
+	unset _CROSS_COMPILE
+else
+	echo $ToolchainCompile
 fi
